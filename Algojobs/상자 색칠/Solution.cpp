fb0133598966bec3c39dbdb6540895b6c04bf9f9
Choiyoonsoo <dbstn1369@gmail.com>
2018-11-22 15:57:29 +0900

    #Algojobs_상자색칠

---
+++ Algojobs/상자 색칠/Solution.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+int main() {
+    
+    
+    int color[1001];
+    
+    int sum =0;
+    int N;
+    
+    scanf("%d" , &N);
+    int array[N];
+    for(int i = 0; i < N; i++){
+      scanf("%d", &array[i]);
+    }
+    
+    for(int i = 0; i< 1001; i++){
+      color[i] = 0;
+    }
+    
+    for(int i = 1 ; i < 1001; i++){
+      for(int j = 0; j < N; j++){
+        if(array[j] == i && color[i] <2){
+          color[i]++; 
+        }
+      }
+    }
+    
+    for(int i = 0 ; i < 1001; i++){
+      sum = sum + color[i];
+    }
+    
+    if(sum >= 6 ){
+      printf("YES");
+    }else{
+      printf("NO");
+    }
+    
+    
+    
+   
+    return 0;
+}
