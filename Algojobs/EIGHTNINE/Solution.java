7da5c3299241df6329993c884227ebf15a77c24b
Choiyoonsoo <dbstn1369@gmail.com>
2018-11-28 00:09:57 +0900

    #Algojobs_EIGHTINE

---
+++ Algojobs/EIGHTNINE/Solution.java
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main() {
+
+    //Please Enter Your Code Here
+    int N,M;
+    
+    scanf("%d %d",&N , &M);
+    
+    int array[N][M];
+    
+    for(int i = 0 ; i< N; i++){
+      for(int j = 0; j<M; j++){
+        scanf("%d" , &array[i][j]);
+      }
+    }
+    
+    for(int i=0; i < N; i++){
+      for(int j = M-1; j >=0; j--){
+        printf("%d ", array[i][j]);
+      }
+      printf("\n");
+    }
+    
+    
+    return 0;
+}
