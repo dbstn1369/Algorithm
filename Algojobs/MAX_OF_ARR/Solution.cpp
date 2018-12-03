75ffbeba549d11aee0e57f34300e313e208be19c
Choiyoonsoo <dbstn1369@gmail.com>
2018-12-03 23:20:26 +0900

    #Algojobs_MAXOFARR

---
+++ Algojobs/MAX_OF_ARR/Solution.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main() {
+
+    //Please Enter Your Code Here
+    int array[10][10];
+    int max = 0;
+    int temp_x;
+    int temp_y;
+    for(int i = 1; i <10; i++){
+      for(int j = 1; j < 10; j++){
+        scanf("%d", &array[i][j]);
+        if(max < array[i][j]){
+          max = array[i][j];
+          temp_x = i;
+          temp_y = j;
+        }
+      }
+    }
+    
+    printf("%d",max);
+    printf("\n%d %d",temp_x, temp_y);
+    
+    
+    
+    return 0;
+}
